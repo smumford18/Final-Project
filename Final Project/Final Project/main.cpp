65457dd9650d164986da03c58e3d7448f1186b3a
Steven Mumford <beaverstever@Stevens-MacBook-Pro.local>
2016-11-15 14:53:51 -0800

    Entered project Ready to go

---
+++ Final Project/Final Project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Final Project
+//
+//  Created by Steven Mumford on 11/15/16.
+//  Copyright © 2016 Steven Mumford. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    
+    //hi eric
+    return 0;
+}

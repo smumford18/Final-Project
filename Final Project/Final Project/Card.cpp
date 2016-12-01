957c2781cadd5882580945b4379460caac0e6180
Eric Klicker <ericklicker@yahoo.com>
2016-12-01 15:35:23 -0800

    Worked 12/1 deck & card class

--- Final Project/Final Project/Card.cpp
+++ Final Project/Final Project/Card.cpp
@@ -5,11 +5,28 @@
 //  Created by Steven Mumford on 11/16/16.
 //  Copyright © 2016 Steven Mumford. All rights reserved.
 //
-
+#include <iostream>
+using namespace std;
 #include "Card.hpp"
 
 Card::Card(string VALUE, string SUIT)
 {
     value = VALUE;
     suit = SUIT;
+}
+
+string Card::getSuit()
+{
+    return suit;
+}
+
+string Card::getValue()
+{
+    return value;
+}
+
+string Card::displayCard()
+{
+    string output = value + " of " + suit;
+    return output;
 }
